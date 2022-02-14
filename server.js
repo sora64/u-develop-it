e94ae2ac1c9abc1cd46eb07bb2278953c9e71daf
Alan Phelps <phelpsa64@gmail.com>
2022-02-14 16:30:42 -0500

    fixed error in server.js of two 'res' values in a use function

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,8 +8,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'C@RCR][D5x_7Bm#-',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // Default response for any other request (Not Found)
-app.use((res, res) => {
+app.use((req, res) => {
     res.status(404).end();
 });
 
